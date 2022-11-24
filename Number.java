c4ddbb7b9a165d1bc6a00ac1df76b97ad4877929
Vaishnavi Lugade <108423518+vaishnavilugade@users.noreply.github.com>
2022-11-24 23:25:54 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,16 @@
+// Write a program to identify of the a number is positive or negative.
+import java.util.*;
+class Number{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("Enter the number:");
+		n=sc.nextInt();
+		if(n>0){
+			System.out.println(n+" is positive number");
+		}
+		else{
+			System.out.println(n+" is negative number");
+		}
+	}
+}
